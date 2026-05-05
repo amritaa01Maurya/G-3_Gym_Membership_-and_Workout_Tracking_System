f171182da68ba687fd19d499d3d8e313068b7b43
amritaa01Maurya <mauryaamrita835@gmail.com>
2026-05-05 13:34:46 +0530

    add intial files

---
+++ backend/gym_ms/src/main/java/com/g_3/gym_ms/GymMsApplication.java
@@ -0,0 +1,13 @@
+package com.g_3.gym_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymMsApplication.class, args);
+	}
+
+}
---
+++ backend/gym_ms/src/test/java/com/g_3/gym_ms/GymMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g_3.gym_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
